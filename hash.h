3faf3fd5a8546d37b61bd9be0481256c73e24c28
hughman98 <30803969+hughman98@users.noreply.github.com>
2024-04-02 14:11:04 -0400

    Removed Comments

--- hash.h
+++ hash.h
@@ -21,23 +21,16 @@ typedef struct UserArray {
 } UserArray;
 
 
-void expand_users(User** users, int size); //covers double pointers and dynamic memory
+void expand_users(User** users, int size); 
 
-bool check_reqs_fixed_size(char* pass); // Checks Password requirments (e.g. correct length, contains a number)
-                                  // User needs to impliment a version
-                                  // of this that is not fixed length.
-                                  // Covers Null bytes for strings, and function prototyping
+bool check_reqs_fixed_size(char* pass); 
 
-// Initial version does not contain Users, and the test taker
-// is expected to edit the function so that a pointer is passed, and access it's member with '->'
-// Tester should also explain why passing a pointer is necessary, e.g. what happens on the heap when a function is called.
-// Also, make the new user structs not be dynamically allocated at first.
-// checks '->' vs '.' and call stack stuff.
-int add_user(UserArray* arr, char* name, char* password);
 
-char * hash_function(char* password, int salt); // Not to be implemented by tester
+int add_user(UserArray arr, char* name, char* password);
 
-bool check_pass(UserArray* arr, char* username, char* password); // Maybe the user should impliment this? It should be pretty straightforward.
+char * hash_function(char* password, int salt); 
+
+bool check_pass(UserArray* arr, char* username, char* password); 
 
 void free_users(UserArray* arr);
 
